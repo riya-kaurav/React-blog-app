eca0e79857a8674167111c95715701ad4cff6d4f
riya-kaurav <kauravriya665@gmail.com>
2025-06-28 23:41:39 +0530

    adding files

---
+++ vite-project/src/components/index.js
@@ -0,0 +1,32 @@
+import Select from './Select';
+import Header from './Header/Header';
+import Footer from  './Footer/Footer';
+import Container from './container/Container';
+import Logo from './Logo';
+import LogoutBtn from './Header/LogoutBtn';
+import PostCard from './PostCard';
+import PostForm from './PostForm';
+import Button from './Button';
+import Input from './Input';
+import RTE from './RTE';
+import Login from './Login';
+import Signup from './Signup';
+import AuthLayout from './AuthLayout';
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    PostCard,
+    PostForm,
+    Button,
+    Input,
+    RTE,
+    Select,
+    Login,
+    Signup,
+    AuthLayout,
+
+}
